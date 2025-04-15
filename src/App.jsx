73a351fa4af95e04fea56e2bd39fee1536ef3c6c
lmf <1351468279@qq.com>
2025-04-15 18:10:59 +0800

    18:10

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,10 @@
 import './App.css'
-
+import Forms from './components/Forms';
 function App(props) {
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <form>
-        <h2 className="label-wrapper">
-          <label htmlFor="new-todo-input" className="label__lg">
-            What needs to be done?
-          </label>
-        </h2>
-        <input
-          type="text"
-          id="new-todo-input"
-          className="input input__lg"
-          name="text"
-          autoComplete="off"
-        />
-        <button type="submit" className="btn btn__primary btn__lg">
-          Add
-        </button>
-      </form>
+      <Forms/>
       <div className="filters btn-group stack-exception">
         <button type="button" className="btn toggle-btn" aria-pressed="true">
           <span className="visually-hidden">Show </span>
